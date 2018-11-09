b118b07cbbd6ee70235cedd43dcdbf9f0145665a
mertsimsek <mertsimsek1992@gmail.com>
2018-11-09 14:48:42 +0300

    Fix extension function syntax error.

--- common/src/main/java/com/trendyol/medusalib/common/extensions/FragmentManagerExtensions.kt
+++ common/src/main/java/com/trendyol/medusalib/common/extensions/FragmentManagerExtensions.kt
@@ -6,48 +6,47 @@ import android.support.v4.app.FragmentTransaction
 
 
 inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) =
-        beginTransaction().func().commitAllowingStateLoss()
+    beginTransaction().func().commitAllowingStateLoss()
 
 fun FragmentManager.commitAdd(containerId: Int, fragment: Fragment, fragmentTag: String) =
-        inTransaction {
-            add(containerId, fragment, fragmentTag)
-        }
+    inTransaction {
+        add(containerId, fragment, fragmentTag)
+    }
 
 
 fun FragmentManager.commitRemove(fragmentTag: String) =
-        findFragmentByTag(fragmentTag)?.let {
-            inTransaction {
-                remove(it)
-            }
+    findFragmentByTag(fragmentTag)?.let {
+        inTransaction {
+            remove(it)
         }
-
+    }
 
 fun FragmentManager.commitAttach(fragmentTag: String) =
-        findFragmentByTag(fragmentTag)?.let {
-            inTransaction {
-                attach(it)
-            }
+    findFragmentByTag(fragmentTag)?.let {
+        inTransaction {
+            attach(it)
         }
+    }
 
 fun FragmentManager.commitDetach(fragmentTag: String) =
-        findFragmentByTag(fragmentTag)?.let {
-            inTransaction {
-              detach(fragment)
-            }
+    findFragmentByTag(fragmentTag)?.let {
+        inTransaction {
+            detach(it)
         }
+    }
 
 fun FragmentManager.commitHide(fragmentTag: String) {
     findFragmentByTag(fragmentTag)?.let {
-            inTransaction {
-              hide(fragment)
-            }
+        inTransaction {
+            hide(it)
+        }
     }
 }
 
 fun FragmentManager.commitShow(fragmentTag: String) {
     findFragmentByTag(fragmentTag)?.let {
         inTransaction {
-              show(fragment)
+            show(it)
         }
     }
 }
