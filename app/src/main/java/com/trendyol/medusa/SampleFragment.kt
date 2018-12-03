f074e8eb301e1ef6e0ae21bfebd270864ecdc826
mertsimsek <mertsimsek1992@gmail.com>
2018-12-03 11:02:38 +0300

    Remove unused imports.

--- app/src/main/java/com/trendyol/medusa/SampleFragment.kt
+++ app/src/main/java/com/trendyol/medusa/SampleFragment.kt
@@ -1,13 +1,11 @@
 package com.trendyol.medusa
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import com.trendyol.medusalib.navigator.Navigator
 
 class SampleFragment : BaseFragment(){
 
