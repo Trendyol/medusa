73cd9ca04deec2b26d45e17164dd648c39d91022
Mert Nevzat Yüksel <mertnevzatyuksel@gmail.com>
2020-04-04 01:14:48 +0300

    remove unused import

--- medusalib/src/test/java/com/trendyol/medusalib/navigator/FragmentStackStateTest.kt
+++ medusalib/src/test/java/com/trendyol/medusalib/navigator/FragmentStackStateTest.kt
@@ -2,7 +2,6 @@ package com.trendyol.medusalib.navigator
 
 import com.google.common.truth.Truth
 import com.trendyol.medusalib.navigator.data.StackItem
-import org.junit.Assert.*
 import org.junit.Test
 import java.util.*
 
