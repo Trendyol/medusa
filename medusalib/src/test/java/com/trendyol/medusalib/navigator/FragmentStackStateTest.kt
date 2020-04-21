b2c24218eda1c94e99c233574f686e72efb1e90f
Mert Nevzat Yüksel <mertnevzatyuksel@gmail.com>
2020-04-21 15:20:26 +0300

    - Fix tab order bug by removing switchTab call in notifyStackItemAdd. - Start calling switchTab in resetCurrentTab function in order to notify stackState. This is needed because clearAllFragments pops tabIndex in tab index stack when fragment stack for that tab is empty. - Set stack count in initializeStackState method in order to create empty fragment stacks at initialization. Previous to this in order to create fragment stacks we were using root fragment provider and calling addStackItem method for each root fragments. - Start initializing root fragments when it is needed, ie. switchTab.

--- medusalib/src/test/java/com/trendyol/medusalib/navigator/FragmentStackStateTest.kt
+++ medusalib/src/test/java/com/trendyol/medusalib/navigator/FragmentStackStateTest.kt
@@ -28,22 +28,4 @@ class FragmentStackStateTest {
 
         Truth.assertThat(stackState.fragmentTagStack).containsExactlyElementsIn(emptyStack)
     }
-
-    @Test
-    fun `given stackstate with tab index stack of (2, 1, 0), when addStackItem is called with tabIndex 1, then new tab index must be (2, 0, 1)`() {
-        val stackState = buildFragmentStackState(tabStack = buildTabStack(2, 1, 0))
-
-        stackState.addStackItem(1, buildStackItem())
-
-        Truth.assertThat(stackState.tabIndexStack).containsExactly(2, 0, 1).inOrder()
-    }
-
-    @Test
-    fun `given stackstate with tab index stack of (2, 1, 0), when addStackItem is called with tabIndex 0, then new tab index must be (2, 1, 0)`() {
-        val stackState = buildFragmentStackState(tabStack = buildTabStack(2, 1, 0))
-
-        stackState.addStackItem(0, buildStackItem())
-
-        Truth.assertThat(stackState.tabIndexStack).containsExactly(2, 1, 0).inOrder()
-    }
 }
