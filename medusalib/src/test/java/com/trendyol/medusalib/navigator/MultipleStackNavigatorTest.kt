e10d17e105ec22b4259e3c19df018e2b0645d39c
Mustafa Berkay Mutlu <berkay.mutlu@trendyol.com>
2023-02-15 17:47:10 +0100

    Add test case that has the bug

---
+++ medusalib/src/test/java/com/trendyol/medusalib/navigator/MultipleStackNavigatorTest.kt
@@ -0,0 +1,35 @@
+package com.trendyol.medusalib.navigator
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
+import com.trendyol.medusalib.TestChildFragment
+import com.trendyol.medusalib.TestParentFragment
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class MultipleStackNavigatorTest {
+
+    @Test
+    fun `given MultipleStackNavigator without an upper fragment, when resetting, should reset to root`() {
+        launchFragmentInContainer<TestParentFragment>().onFragment {
+            val sut = MultipleStackNavigator(
+                fragmentManager = it.childFragmentManager,
+                containerId = TestParentFragment.CONTAINER_ID,
+                rootFragmentProvider = listOf(
+                    { TestChildFragment.newInstance("instance 1") },
+                    { TestChildFragment.newInstance("instance 2") },
+                    { TestChildFragment.newInstance("instance 3") },
+                ),
+            )
+            sut.initialize(null)
+
+            sut.reset(tabIndex = 0, resetRootFragment = false)
+
+            assertThat(sut.hasOnlyRoot(0)).isTrue()
+        }
+    }
+}
