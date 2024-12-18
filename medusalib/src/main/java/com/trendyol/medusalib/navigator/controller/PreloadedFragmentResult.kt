77cc0150df30503201ce737f37f04e4848069c85
erolaksoy <erolaksoy98@gmail.com>
2024-12-18 18:06:47 +0300

    Add Javadoc to PreloadedFragmentResult

--- medusalib/src/main/java/com/trendyol/medusalib/navigator/controller/PreloadedFragmentResult.kt
+++ medusalib/src/main/java/com/trendyol/medusalib/navigator/controller/PreloadedFragmentResult.kt
@@ -1,10 +1,22 @@
 package com.trendyol.medusalib.navigator.controller
 
+/**
+ * Represents the result of attempting to show a preloaded fragment.
+ */
 sealed interface PreloadedFragmentResult {
 
-    data object Success: PreloadedFragmentResult
+    /**
+     * Indicates that the preloaded fragment was successfully shown.
+     */
+    data object Success : PreloadedFragmentResult
 
-    data object FallbackSuccess: PreloadedFragmentResult
+    /**
+     * Indicates that a fallback fragment was used and successfully shown.
+     */
+    data object FallbackSuccess : PreloadedFragmentResult
 
-    data object NotFound: PreloadedFragmentResult
+    /**
+     * Indicates that neither the preloaded fragment nor a fallback fragment was found.
+     */
+    data object NotFound : PreloadedFragmentResult
 }
