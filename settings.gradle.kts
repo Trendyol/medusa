c233c2bae9f629d02dfc77545c53f8346586381d
mert.yuksel <mert.yuksel@trendyol.com>
2025-10-01 16:27:14 +0200

    Update publish step

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+rootProject.name = "Medusa"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven {
+            url = uri("https://repository.kotzilla.io/repository/Koin-Embedded/")
+
+            content {
+                includeModule("io.insert-koin", "embedded-koin-core")
+                includeModule("io.insert-koin", "embedded-koin-core-jvm")
+                includeModule("io.insert-koin", "embedded-koin-android")
+            }
+        }
+    }
+    versionCatalogs {
+        create("libs") { from(files("gradle/libs.toml")) }
+    }
+}
+
+include(":medusalib")
+include(":app")
